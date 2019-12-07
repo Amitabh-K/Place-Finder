26b5810c222e0cffd646c8d5c3b3e358463a8a17
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-12-06 21:46:09 -0500

    adding input from serch

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var map;
+var map, infowindow;
 
 function createMap () {
   var options = {
     center: { lat: 43.557930, lng: -79.707533 },
-    zoom: 5
+    zoom: 10,
+    
   };
   map = new google.maps.Map(document.getElementById('map'), options);
 
+
+  var input = document.getElementById('search');
+  var searchBox = new google.maps.places.SearchBox(input);
 
 
 
